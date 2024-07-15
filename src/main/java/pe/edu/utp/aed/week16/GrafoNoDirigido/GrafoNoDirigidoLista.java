aa5d71e0704edae16392e8f38e28b873fa0666c2
Yoshii2208 <luis.2298leo@gmail.com>
2024-07-15 15:04:41 -0500

    ADD_Recorrido

--- src/main/java/pe/edu/utp/aed/week16/GrafoNoDirigido/GrafoNoDirigidoLista.java
+++ src/main/java/pe/edu/utp/aed/week16/GrafoNoDirigido/GrafoNoDirigidoLista.java
@@ -1,7 +1,6 @@
 package pe.edu.utp.aed.week16.GrafoNoDirigido;
 
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.*;
 
 public class GrafoNoDirigidoLista {
     LinkedList<String>[] arreglo;
@@ -21,5 +20,82 @@ public class GrafoNoDirigidoLista {
         int idxFinal = Arrays.binarySearch(nodos, _final);
         arreglo[idxInicial].add(nodos[idxFinal]);
         arreglo[idxFinal].add(nodos[idxInicial]);
+    }
+
+    public void recorridoPorAmplitud(String root) {
+        Set<String> visitados = new LinkedHashSet<>();
+        Queue<String> cola = new LinkedList<>();
+        cola.offer(root);
+        visitados.add(root);
+
+        while (!cola.isEmpty()) {
+            String u = cola.poll();
+            System.out.println(u);
+            int idx = Arrays.binarySearch(nodos, u);
+            for (String v : arreglo[idx]) {
+                System.out.print(v + ",");
+                if (!visitados.contains(v)) {
+                    visitados.add(v);
+                    cola.offer(v);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public LinkedList<LinkedList<String>> recorridoPorAmplitud(String root, String destino) {
+        LinkedList<LinkedList<String>> rutas = new LinkedList<>();
+
+        Set<String> visitados = new LinkedHashSet<>();
+        Queue<String> cola = new LinkedList<>();
+        cola.offer(root);
+        visitados.add(root);
+
+        LinkedList<String> ruta = new LinkedList<>();
+        ruta.add(root);
+        rutas.add(ruta);
+
+        while (!cola.isEmpty()) {
+            String u = cola.poll();
+            int idx = Arrays.binarySearch(nodos, u);
+            for (String v : arreglo[idx]) {
+                if (!ruta.contains(v))
+                    ruta.add(v);
+                else {
+                    LinkedList<String> nuevaRuta = new LinkedList<>();
+                    nuevaRuta.add(root);
+                    rutas.add(nuevaRuta);
+                }
+
+                if (!visitados.contains(v) && !v.equals(destino)) {
+                    visitados.add(v);
+                    cola.offer(v);
+                } else {
+                    return rutas;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public void recorridoEnProfundidad(String root) {
+        Set<String> visitados = new LinkedHashSet<>();
+        Stack<String> pila = new Stack<>();
+        pila.push(root);
+
+        while(!pila.isEmpty()) {
+            String u = pila.pop();
+            System.out.println(u);
+            int idx = Arrays.binarySearch(nodos, u);
+            if(!visitados.contains(u)) {
+                visitados.add(u);
+                for(String v: arreglo[idx]) {
+                    System.out.print(v + ",");
+                    pila.push(v);
+                }
+                System.out.println();
+            }
+        }
     }
 }
---
+++ src/main/java/pe/edu/utp/aed/week16/GrafoNoDirigido/PruebaRecorridoGrafo.java
@@ -0,0 +1,19 @@
+package pe.edu.utp.aed.week16.GrafoNoDirigido;
+
+public class PruebaRecorridoGrafo {
+    public static void main(String[] args) {
+
+        String[] nodos = {"A", "B", "C", "D", "E"};
+
+        GrafoNoDirigidoLista grafo = new GrafoNoDirigidoLista(nodos);
+        grafo.agregarEnlace("A", "B");
+        grafo.agregarEnlace("A", "C");
+        grafo.agregarEnlace("B", "C");
+        grafo.agregarEnlace("B", "D");
+        grafo.agregarEnlace("B", "E");
+        grafo.agregarEnlace("D", "E");
+
+        grafo.recorridoPorAmplitud("A", "E");
+        //grafo.recorridoEnProfundidad("A");
+    }
+}
